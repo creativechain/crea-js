d59c77b7e7a9d9502e12a2eccf5780c1a7d373c6
ander7agar <ander7agar@gmail.com>
2018-09-20 11:03:58 +0200

    fix rpc-auth dependency

--- src/api/index.js
+++ src/api/index.js
@@ -17,7 +17,7 @@ import {
 } from './transports/http';
 import {
     sign as signRequest
-} from '@creativechain/rpc-auth';
+} from '@steemit/rpc-auth';
 
 class Steem extends EventEmitter {
     constructor(options = {}) {
